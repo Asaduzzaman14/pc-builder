7f4ed20bb5b9d701a394fb93698daee992bd57b1
Md Asaduzaman <asadasad9282@gmail.com>
2023-08-01 20:10:40 +0600

    footer

--- src/components/Ui/Banner.js
+++ src/components/Ui/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import banner from '@/assets/pc-builder.jpg'
+import Image from 'next/image';
 
 const Banner = () => {
     return (
         <div>
-            <h2>BANNER</h2>
+            <Image src={banner} height={500} width={'100%'} alt="" />
         </div>
     );
 };
--- src/components/Ui/Footer.js
+++ src/components/Ui/Footer.js
@@ -3,7 +3,31 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>Footer</h2>
+            <footer className="grid md:grid-cols-2 lg:grid-cols-4 gap-5 p-10 bg-base-200 text-base-content">
+                <div>
+                    <p>Providing reliable tech since 2020</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <p className="link link-hover">Branding</p>
+                    <p className="link link-hover">Design</p>
+                    <p className="link link-hover">Marketing</p>
+                    <p className="link link-hover">Advertisement</p>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <p className="link link-hover">About us</p>
+                    <p className="link link-hover">Contact</p>
+                    <p className="link link-hover">Jobs</p>
+                    <p className="link link-hover">Press kit</p>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <p className="link link-hover">Terms of use</p>
+                    <p className="link link-hover">Privacy policy</p>
+                    <p className="link link-hover">Cookie policy</p>
+                </div>
+            </footer>
         </div>
     );
 };
