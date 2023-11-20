1e75807aa1d1e2c7605d3f6909462e9be7770bcd
Md Asaduzaman <asadasad9282@gmail.com>
2023-11-20 23:27:15 +0600

    completed all category pages

--- src/pages/category/ssd.js
+++ src/pages/category/ssd.js
@@ -1,10 +1,17 @@
 import RootLayout from "@/components/Layoutes/RootLayout";
+import CatagoryProduct from "@/components/Ui/catagoryProduct";
 import React from "react";
 
-const Ssd = () => {
+const Ssd = ({ products }) => {
   return (
     <div>
-      <h2>SSD</h2>
+      <h2 className='py-2'>SSD</h2>
+
+      <div className='grid lg:grid-cols-2 gap-2'>
+        {products?.map((part) => {
+          return <CatagoryProduct key={part?._id} product={part} />;
+        })}
+      </div>
     </div>
   );
 };
@@ -13,4 +20,15 @@ export default Ssd;
 
 Ssd.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getServerSideProps = async () => {
+  const result = await fetch(
+    "https://pc-builder-gules-psi.vercel.app/api/v1/pc-parts?Category=PSU"
+  );
+  const data = await result.json();
+  console.log(data);
+  return {
+    props: { products: data?.data },
+  };
 };
