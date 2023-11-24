5d5e0fea197eee13d4422d95159486793a9ed5f0
Md Asaduzaman <asadasad9282@gmail.com>
2023-11-24 23:30:17 +0600

    banner update

--- src/components/Ui/Banner.js
+++ src/components/Ui/Banner.js
@@ -1,22 +1,17 @@
 import React from "react";
 import img1 from "@/assets/pc-banner-1.jpg";
-import img2 from "@/assets/pc-banner-2.jpg";
-import img3 from "@/assets/pc-banner-3.jpg";
-import img from "@/assets/img-2.jpg";
+import img2 from "../../assets/pc-banner-2.jpg";
+import img3 from "../../assets/pc-banner-3.jpg";
+import img5 from "@/assets/img-2.jpg";
 import Image from "next/image";
 import { Carousel } from "antd";
 
 const Banner = () => {
   const contentStyle = {
-    // height: "160px",
-    color: "#fff",
-    // lineHeight: "160px",
-    textAlign: "center",
-    // background: "#364d79",
-    width: "1200px",
+    maxHeight: "100vh",
   };
   const settings = {
-    className: "mt-5 text-white text-left",
+    className: "mt-2 lg:mt-4 text-white text-left",
     autoplay: true,
     dots: false,
     speed: 800,
@@ -24,34 +19,32 @@ const Banner = () => {
 
   return (
     <div>
-      <div style={contentStyle}>
-        <Image
-          className='w-full'
-          alt='aa'
-          // width={"500"}
-          height={500}
-          src={img2}
-        />
-      </div>
-      {/* <Carousel {...settings}>
-        <div>
-          <div style={contentStyle}>
+      <Carousel {...settings} style={contentStyle}>
+        <div style={contentStyle}>
+          <div>
             <Image
-              className='w-full'
-              alt='aa'
-              // width={"500"}
-              height={500}
-              src={img2}
+              src={img1}
+              alt='Your Image Alt Text'
+              width={350}
+              height={200}
+              layout='responsive'
+              className='max-h-screen'
             />
           </div>
         </div>
         <div>
-          <div style={contentStyle}>
-            <Image alt='aa' width={"1200"} height={500} src={img3} />
+          <div>
+            <Image
+              src={img3}
+              alt='Your Image Alt Text'
+              width={350}
+              height={200}
+              layout='responsive'
+              className='max-h-screen'
+            />
           </div>
         </div>
-      </Carousel> */}
-      {/* <Image src={banner} height={500} width={'100%'} alt="" /> */}
+      </Carousel>
     </div>
   );
 };
