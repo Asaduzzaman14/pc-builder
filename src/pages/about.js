becb3a4edbdf90b53783a93c28892c21f4eac466
Md Asaduzaman <asadasad9282@gmail.com>
2023-12-17 22:04:31 +0600

    relode issue fixed

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,31 @@
-import RootLayout from '@/components/Layoutes/RootLayout';
-import React from 'react';
+import RootLayout from "@/components/Layoutes/RootLayout";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase.auth";
 
 const About = () => {
-    return (
-        <div style={{ textAlign: "center" }}>
-            <h2>About</h2>
-        </div>
-    );
+  const { data: session } = useSession();
+  const [user, loadign, err] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session && !user) {
+      router?.push("/login");
+      // redirect("/blogs");
+    }
+  }, [router, session, user]);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>About</h2>
+    </div>
+  );
 };
 
 export default About;
 
 About.getLayout = function getLayout(page) {
-    return <RootLayout>{page}</RootLayout>;
+  return <RootLayout>{page}</RootLayout>;
 };
