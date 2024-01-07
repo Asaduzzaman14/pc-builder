99260a8516a87722ccf54c2a4cefcc6298b1d2ac
Md Asaduzaman <asadasad9282@gmail.com>
2024-01-07 23:55:44 +0600

    create component for dahsboard layout

--- src/components/Layoutes/DashboardLayout.js
+++ src/components/Layoutes/DashboardLayout.js
@@ -22,67 +22,16 @@ const DashboardLayout = ({ children }) => {
         <div className=' fixed  top-15 left-0  drawer-side h-[80%]'>
           <label htmlFor='dashboard-sidebar' className='drawer-overlay'></label>
           <ul className='overflow-y-scroll bg-primary/95 text-white font-semibold menu  w-40 h-screen'>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='addproduct'>Add Product</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allOrders'>Orders</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allUsers'>Users</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allreviews'>Reviews</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>Products</Link>
-            </li>
-            <li className='hover:bg-[#1a192cc9]'>
-              <Link href='allproducts'>11111</Link>
-            </li>
-            <li>
-              <Link href='/' className='mb-14 mx-auto'>
-                <button className=' border px-4 mx-auto m-2 rounded-md w-fit'>
-                  Home
-                </button>
-              </Link>
-            </li>
+            <Header />
           </ul>
         </div>
 
-        <div className='  ml-40 justify-end   border-2  '>
+        <div className='  ml-40 justify-end   -2  '>
           <div className='mx-auto'>{children}</div>
 
           <div className='min-w-full'>
-            <div className='p-3 min-w-full'>
-              <h2 className='text-2xl'>
-                Footer Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident nihil assumenda quisquam molestias commodi a, quam
-              </h2>
+            <div className=' pt-4 min-w-full'>
+              <Footer />
             </div>
           </div>
         </div>
--- src/components/dashboardUi/Header.js
+++ src/components/dashboardUi/Header.js
@@ -1,4 +1,5 @@
 import { Button } from "antd";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -9,17 +10,55 @@ const Header = (props) => {
     router.push("/");
   };
   return (
-    <div className='bg-gray-900  max-h-screen  h-fit w-full py-4 '>
-      <div className='flex justify-between  text-white px-4'>
-        <h2 className='text-white text-2xl'>Logo</h2>
-
-        <div className='flex gap-3'>
-          <Button onClick={handleClick} type='primary' danger>
-            home
-          </Button>
-          <h2 className=''>User</h2>
-          <p className='border border-1 rounded-md px-2 p-1'> Menu</p>
-        </div>
+    <div className=' '>
+      <div className=' flex flex-col gap-2 ps-2'>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allOrders'>Orders 1</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allUsers'>Users 2</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allreviews'>Reviews 3</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>Products</Link>
+        </li>
+        <li className='hover:bg-[#1a192cc9]'>
+          <Link href='allproducts'>11111</Link>
+        </li>
+        <Button onClick={handleClick} type='primary' danger>
+          home
+        </Button>
+        <Link href='/' className='mb-14 mx-auto'>
+          <button className=' border px-4 mx-auto m-2 rounded-md w-fit'>
+            Home
+          </button>
+        </Link>
       </div>
     </div>
   );
